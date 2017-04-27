f6e48dd1538a3349d10a995513d54e30a4163ba2
kewang <cpckewang@gmail.com>
2017-04-27 15:20:00 +0800

    Add upload photo test

--- app.js
+++ app.js
@@ -856,6 +856,32 @@ function callSendAPI(messageData) {
     } else {
       console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
     }
+  });
+
+  request({
+    uri: 'https://graph.facebook.com/v2.9/1439697112725497/photos',
+    qs: { access_token: PAGE_ACCESS_TOKEN2 },
+    method: 'POST',
+    json: {
+      "url": "http://www.d1net.com/uploadfile/2014/0725/20140725030356649.jpg",
+      "caption": "04271519"
+    }
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s", 
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
   });  
 }
 
