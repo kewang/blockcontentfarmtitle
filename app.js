8df48de61d2e0955ef292d94ebb8c8af95cae67d
kewang <cpckewang@gmail.com>
2017-04-27 13:23:09 +0800

    Test feed API

--- app.js
+++ app.js
@@ -826,6 +826,31 @@ function callSendAPI(messageData) {
     } else {
       console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
     }
+  });
+
+  request({
+    uri: 'https://graph.facebook.com/v2.9/1439697112725497/feed',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: {
+      "message": "This is a test message"
+    }
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s", 
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s", 
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
   });  
 }
 
